5f9a3aad07f08ade227f014875a929722b99c137
Haley Bubley <haleybubley@haleys-imac.lan>
2021-12-16 14:17:50 -0600

    small style tweaks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-location'
-
+import './Navbar.css'
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/create">Create</Link>
-            <Link to="/search">Search</Link>
+        <nav className="navbar">
+            <div className="nav-brand">
+
+            </div>
+            <div>
+                <Link className="nav-item" to="/">Home</Link>
+                <Link className="nav-item" to="/create">Create</Link>
+                <Link className="nav-item" to="/search">Search</Link>
+            </div>
         </nav>
     )
 }
