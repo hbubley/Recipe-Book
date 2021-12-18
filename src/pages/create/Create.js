daf9aac893c2495e823c22d725e387436c87a257
Haley Bubley <haleybubley@haleys-imac.lan>
2021-12-18 17:01:35 -0600

    may change how rows are handled.

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -4,11 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [method, setMethod] = useState('');
     const [cookingTime, setCookingTime] = useState('');
-
+    const [ingredients, setIngredients] = useState([{}])
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(title, method, cookingTime)
     }
+
+    const handleRowChange = (value, index) => {
+        const ingredientsCopy = [...ingredients];
+        ingredientsCopy[index] = { ...ingredientsCopy[index], value: value };
+        console.log("🚀 ~ file: Create.js ~ line 16 ~ handleRowChange ~ ingredientsCopy", ingredientsCopy)
+        setIngredients(ingredientsCopy);
+    }
+    const addRow = () => {
+        const ingredientsCopy = [...ingredients, { value: "" }];
+        setIngredients(ingredientsCopy)
+    }
+
     return (
         <div className="create">
             <h2 className="page-title">Add a New Recipe</h2>
@@ -19,15 +31,35 @@ const Create = () => {
                         type="text"
                         onChange={(e) => setTitle(e.target.value)}
                         value={title}
-                        required
                     />
+                </label>
+                <label>
+                    <span>Recipe ingredients:</span>
+                    <div className="ingredients">
+                        {ingredients.map((ingredient, index) => {
+                            return (
+                                <div className="ingredient">
+                                    <span className="ingredient-input">
+                                        <input
+                                            type="text"
+                                            onChange={(e) => handleRowChange(e.target.value, index)}
+                                            value={ingredient?.value ? ingredient.value : ""}
+                                        />
+                                    </span>
+                                    <span className="ingredient-delete">
+                                        <button className="delete-ingredient">x</button>
+                                    </span>
+                                </div>
+                            )
+                        })}
+                        <button className="button" onClick={addRow}><p>Add another</p></button>
+                    </div>
                 </label>
                 <label>
                     <span>Recipe method:</span>
                     <textarea
                         onChange={(e) => setMethod(e.target.value)}
                         value={method}
-                        required
                     />
                 </label>
                 <label>
@@ -36,7 +68,6 @@ const Create = () => {
                         type="number"
                         onChange={(e) => setCookingTime(e.target.value)}
                         value={cookingTime}
-                        required
                     />
                 </label>
                 <button className="button"><p>Submit</p></button>
